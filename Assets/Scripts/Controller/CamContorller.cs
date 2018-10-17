acdc9d5e1cd69ffb98b69661c776feeddc13a4ab
ramonn334 <ramonn334@gmail.com>
2018-10-17 20:30:51 +0300

    T-2 Camera zooming T-4 Limit camera zooming

--- Assets/Scripts/Controller/CamContorller.cs
+++ Assets/Scripts/Controller/CamContorller.cs
@@ -5,51 +5,36 @@ using UnityEngine;
 
 public class CamContorller : MonoBehaviour {
     public float Speed { get; } = 1.0f;
+    public float ZoomSpeed { get; } = 1.0f;
+    public float OrthographicSize
+    {
+        get => Camera.main.orthographicSize;
+        set
+        {
+            if (value > 20.0f) Camera.main.orthographicSize = 20.0f;
+            else if (value < 8.0f) Camera.main.orthographicSize = 8.0f;
+            else Camera.main.orthographicSize = value;
+        }
+    }
+
     [SerializeField]
     private float _speed;
-    private Vector3 _startPos;
-    private Vector3 _direction;
-    private Vector3 _startTransformPos;
-    private Vector3 _endTransformPos;
-
 
     private Vector3 m_startTransformPosition;
     private Vector3 m_currentTransformPosition;
     private Vector3 m_movementDirection;
+
+    private float m_distanceBetweenTwoTouches = 0;
 
     private int _clickCount;
 	// Use this for initialization
 	void Start () {
         Debug.Log("CamController init.");
         Debug.Log(name);
-        _clickCount = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    _startPos = Input.mousePosition;
-        //    _startTransformPos = Camera.main.ScreenPointToRay(_startPos).origin;
-        //    Debug.Log(String.Format("Start position: {0}", _startPos));
-
-        //}
-
-        //if (Input.GetMouseButton(0))
-        //{
-        //    var currPos = Input.mousePosition;
-        //    var currTransformPos = Camera.main.ScreenPointToRay(currPos).origin;
-        //    _direction = _startPos - currPos;
-        //    var inverseCurPos = _startPos + _direction; 
-        //    if (!_direction.Equals(Vector3.zero))
-        //    {
-        //        var directionWorldSpace = _startTransformPos - currTransformPos;
-        //        Debug.Log(String.Format("Direction: {0}", _direction.normalized));
-        //        transform.localPosition += directionWorldSpace * Speed;
-        //        _startPos = currPos;
-        //    }
-        //}
         if (Input.touchCount == 1)
         {
             var touch = Input.GetTouch(0);
@@ -61,15 +46,41 @@ public class CamContorller : MonoBehaviour {
                 case TouchPhase.Moved:
                     m_currentTransformPosition = Camera.main.ScreenPointToRay(touch.position).origin;
                     m_movementDirection = m_startTransformPosition - m_currentTransformPosition;
+                    Debug.Log(m_movementDirection);
                     transform.localPosition += m_movementDirection * Speed;
                     break;
+                //case TouchPhase.Stationary:
+                //    m_movementDirection = Vector3.zero;
+                //    break;
             }
         }
 
         if (Input.touchCount == 2)
         {
-            var firstTouch = Input.GetTouch(0);
-            var secondTouch = Input.GetTouch(1);
+            var fTouch = Input.GetTouch(0);
+            var sTouch = Input.GetTouch(1);
+            var fStartPos = Vector3.zero;
+            var sStartPos = Vector3.zero;
+
+            if (fTouch.phase == TouchPhase.Began && sTouch.phase == TouchPhase.Began)
+            {
+                m_distanceBetweenTwoTouches = Vector2.Distance(fTouch.position, sTouch.position);
+            }
+
+            if (fTouch.phase == TouchPhase.Moved || sTouch.phase == TouchPhase.Moved)
+            {
+                var curDistance = Vector2.Distance(fTouch.position, sTouch.position);
+                if (m_distanceBetweenTwoTouches > curDistance)
+                {
+                    OrthographicSize += ZoomSpeed;
+                }
+                else if (m_distanceBetweenTwoTouches < curDistance)
+                {
+                    OrthographicSize -= ZoomSpeed;
+                }
+
+                m_distanceBetweenTwoTouches = curDistance;
+            }
 
             Debug.Log(Input.touchCount);
         }
